70882f85fb3b9bd1169c5988f0ff9a80037fb3ee
Syeda Sara <syeda.sara@genexinfosys.com>
2023-05-05 00:03:43 +0600

    auth guard not working

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UseGuards(AuthGuard)
   @Get('profile')
   profile() {
     return 'User info';
